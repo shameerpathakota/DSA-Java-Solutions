6b53a848e2c1d443d4217728f723efa06eeae68f
Shameer Pathakota <shameerpathakota@gmail.com>
2026-06-25 16:44:41 +0530

    Time: 0 ms (100%), Space: 42.2 MB (17%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -0,0 +1,52 @@
+class Solution {
+    public int climbStairs(int n) {
+        if(n <= 1){
+            return 1;
+        }
+
+        int prev2 = 1;
+        int prev = 1;
+
+        for(int i = 2; i <= n; i++){
+            int curri = prev + prev2;
+            prev2 = prev;
+            prev = curri;
+        }
+
+        return prev;
+
+    }
+
+    // public int secondway(int n){
+    //     if(n <= 1){
+    //         return 1;
+    //     }
+    //     int[] dp = new int[n+1];
+    //     dp[0] = 1;
+    //     dp[1] = 1;
+
+    //     for(int i = 2; i <= n; i++){
+    //         dp[i] = dp[i-1] + dp[i-2];
+    //     }
+
+    //     return dp[n];
+    // }
+
+    // public int helper(int n, int[] dp){
+    //     if(n == 0 || n == 1){
+    //         return 1;
+    //     }
+        
+    //     if(dp[n] != -1){
+    //         return dp[n];
+    //     }
+
+    //     int onejump = climbStairs(n-1);
+    //     int twojumps = climbStairs(n-2);
+
+    //     return  dp[n] = onejump + twojumps;
+
+
+        
+    // }
+}
