49ab43715c12c94e398c822c1446f9f58fd2ccfb
Shameer Pathakota <shameerpathakota@gmail.com>
2026-06-26 07:38:56 +0530

    Time: 7 ms (91.54%), Space: 46.7 MB (88.82%) - LeetHub

--- 3737-count-subarrays-with-majority-element-i/3737-count-subarrays-with-majority-element-i.java
+++ 3737-count-subarrays-with-majority-element-i/3737-count-subarrays-with-majority-element-i.java
@@ -1,19 +1,75 @@
 class Solution {
     public int countMajoritySubarrays(int[] nums, int target) {
+         int n = nums.length;
+        int[] arr = new int[n];
+        for(int i = 0; i < n; i++){
+            if(nums[i] == target){
+                arr[i] = 1;
+            }
+            else{
+                arr[i] = -1;
+            }
+        }
+
+        int[] prefixSum = new int[n+1];
+        prefixSum[0] = 0;
+        for(int k = 1; k < n+1; k++){
+            prefixSum[k] = prefixSum[k-1] + arr[k-1];
+        }
+
+        return mergeSort(prefixSum, 0, n);
+    }
+
+    int mergeSort(int[] nums, int left, int right){
         int count = 0;
-        for(int i = 0; i < nums.length; i++){
-            int freq = 0;
-            for(int j = i; j < nums.length; j++){
-                if(nums[j] == target){
-                    freq++;
-                }
-                int length = j - i + 1;
-                if(freq > length/2){
-                    count++;
-                }
-            } 
+        if(right > left){
+            int mid = left + (right - left)/2;
+            count = mergeSort(nums, left, mid);
+            count += mergeSort(nums, mid+1, right);
+            count += merge(nums, left, mid, right);
         }
 
         return count;
+    }
+
+    int merge(int[] nums, int left, int mid, int right){
+        int count = 0;
+        int i = left;
+        for(int j = mid+1; j <= right; j++){
+            while(i <= mid && nums[i] < nums[j]){
+                i++;
+            }
+            count += (i-left);
+        }
+
+        int a = left;
+        int b = mid+1;
+        int k = 0;
+        int[] temp = new int[right-left+1];
+
+        while(a <= mid && b <= right){
+            if(nums[a] <= nums[b]){
+                temp[k++] = nums[a++];
+            }
+            else{
+                temp[k++] = nums[b++];
+            }
+        }
+
+        while(a <= mid){
+            temp[k++] = nums[a++];
+        }
+
+        while(b <= right){
+            temp[k++] = nums[b++];
+        }
+
+        
+        for(k = 0, a = left; k < temp.length; k++, a++){
+            nums[a] = temp[k];
+        }
+
+        return count;
+
     }
 }
