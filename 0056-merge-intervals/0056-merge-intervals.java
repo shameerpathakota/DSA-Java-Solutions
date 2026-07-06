8bf16e782b8d5909d8d35c262f8dd30c87363414
Shameer Pathakota <shameerpathakota@gmail.com>
2026-07-06 22:22:53 +0530

    Time: 12 ms (9.73%), Space: 49 MB (59.77%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.java
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -6,20 +6,19 @@ class Solution {
         Arrays.sort(intervals, Comparator.comparingDouble(a -> a[0]));
 
         List<int[]> list = new ArrayList<>();
-        int[] newinterval = intervals[0];
+        int[] prev = intervals[0];
 
-        list.add(newinterval);
-
-        for(int[] interval : intervals){
-            if(interval[0] <= newinterval[1]){
-                newinterval[1] = Math.max(newinterval[1], interval[1]);
+        for(int i = 1; i < intervals.length; i++){
+            int[] inter = intervals[i];
+            if(inter[0] <= prev[1]){
+                prev[1] = Math.max(prev[1], inter[1]);
             }
             else{
-                newinterval = interval;
-                list.add(newinterval);
+                list.add(prev);
+                prev = inter;
             }
         }
-
+        list.add(prev);
         return list.toArray(new int[list.size()][]);
     }
 }
