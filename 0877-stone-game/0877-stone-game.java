ee010657864d5bfd61033a9102e52dd3878f6ad2
Shameer Pathakota <shameerpathakota@gmail.com>
2026-08-02 10:32:16 +0530

    Time: 0 ms (100%), Space: 43 MB (76.69%) - LeetHub

--- 0877-stone-game/0877-stone-game.java
+++ 0877-stone-game/0877-stone-game.java
@@ -1,33 +1,5 @@
 class Solution {
     public boolean stoneGame(int[] piles) {
-        int[][] dp = new int[501][501];
-        for(int[] arr : dp){
-            Arrays.fill(arr, -1);
-        }
-        int n = piles.length;
-        int sum = 0;
-        for(int num : piles){
-            sum += num;
-        }
-
-        int alice_score = solve(piles, 0, n-1, dp);
-
-        return (alice_score > sum/2);
-    }
-
-    public int solve(int[] nums, int i, int j, int[][]dp){
-
-        if(i > j){
-            return 0;
-        }
-
-        if(dp[i][j] != -1){
-            return dp[i][j];
-        }
-
-        int take_i = nums[i] + Math.min(solve(nums, i+2, j, dp), solve(nums, i+1, j-1, dp));
-        int take_j = nums[j] + Math.min(solve(nums, i+1, j-1, dp), solve(nums, i, j-2, dp));
-
-        return dp[i][j] = Math.max(take_i, take_j);
+        return true;
     }
 }
