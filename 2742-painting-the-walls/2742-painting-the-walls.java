34f1e9a701a656a7c366c042c7aea7bc983da714
Shameer Pathakota <shameerpathakota@gmail.com>
2026-08-07 22:31:20 +0530

    Time: 363 ms (5%), Space: 115.7 MB (5%) - LeetHub

---
+++ 2742-painting-the-walls/2742-painting-the-walls.java
@@ -0,0 +1,29 @@
+class Solution {
+    int n;
+    public int paintWalls(int[] cost, int[] time) {
+        int[][] dp = new int[501][501];
+        for(int[] arr : dp){
+            Arrays.fill(arr, -1);
+        }
+        n = time.length;
+        return solve(cost, time, 0, n, dp);
+    }
+
+    int solve(int[] cost, int[] time, int index, int remaining, int[][] dp){
+        if(remaining <= 0){
+            return 0;
+        }
+        if(index >= n){
+            return 1_000_000_000;//Integer.MAX_VALUE may cause the integer overflow so instead of that use the above one
+        }
+
+        if(dp[index][remaining] != -1){
+            return dp[index][remaining];
+        }
+
+        int paint_idx = cost[index] + solve(cost, time, index+1, remaining-1-time[index], dp);
+        int no_paint = solve(cost, time, index+1, remaining, dp);
+
+        return dp[index][remaining] = Math.min(paint_idx, no_paint);
+    }
+}
