56dec947918d593796599832a565e84e15955434
Shameer Pathakota <shameerpathakota@gmail.com>
2026-08-25 10:20:13 +0530

    Time: 6 ms (31.2%), Space: 86.4 MB (75.64%) - LeetHub

--- 1872-stone-game-viii/1872-stone-game-viii.java
+++ 1872-stone-game-viii/1872-stone-game-viii.java
@@ -1,13 +1,38 @@
 class Solution {
-    public int stoneGameVIII(int[] A) {
-        int n = A.length;
-        for (int i = 1; i < n; i++)
-            A[i] += A[i - 1];
+    int n;
+    public int stoneGameVIII(int[] stones) {
+        n = stones.length;
+        int[] prefix = new int[n];
+        prefix[0] = stones[0];
 
-        int ans = A[n - 1];
-        for (int i = n - 2; i > 0; i--)
-            ans = Math.max(ans, A[i] - ans);
+        for(int i = 1; i < n; i++){
+            prefix[i] = prefix[i-1] + stones[i];
+        }
 
-        return ans;
+        int[] dp = new int[n];
+        dp[n-1] = prefix[n-1];
+        for(int i = n-2; i >= 1; i--){
+            int take = prefix[i] - dp[i+1];
+            int skip = dp[i+1];
+
+            dp[i] = Math.max(take, skip);
+        }
+
+        return dp[1];
     }
+
+    // int solve(int i, int[] prefix, int[] dp){
+    //     if(i == n-1){
+    //         return prefix[n-1];
+    //     }
+
+    //     if(dp[i] != -1){
+    //         return dp[i];
+    //     }
+
+    //     int take = prefix[i] - solve(i+1, prefix, dp);
+    //     int skip = solve(i+1, prefix, dp);
+
+    //     return dp[i] = Math.max(take, skip);
+    // }
 }
