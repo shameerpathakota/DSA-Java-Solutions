8176047f96d5bcf239f0c08c57ccc2c78b1edcc7
Shameer Pathakota <shameerpathakota@gmail.com>
2026-08-31 14:17:19 +0530

    Time: 2 ms (91.87%), Space: 47.8 MB (57.55%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -3,14 +3,16 @@ class Solution {
         int slow = 0;
         for(int i = 0; i < nums.length; i++){
             if(nums[i] != 0){
-                swap(nums,slow,i);
+                swap(nums, i, slow);
                 slow++;
             }
         }
     }
-    public void swap(int[] arr, int start, int end){
-        int temp = arr[start];
-        arr[start] = arr[end];
-        arr[end] = temp;
+
+    void swap(int[] nums, int i, int j){
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+        return;
     }
 }
