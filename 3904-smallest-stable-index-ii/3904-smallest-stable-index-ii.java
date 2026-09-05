258fa6a20d45e5d410d0fa269772a213b365d30f
Shameer Pathakota <shameerpathakota@gmail.com>
2026-09-05 06:23:07 +0530

    Time: 4 ms (88.55%), Space: 133.3 MB (14.89%) - LeetHub

--- 3904-smallest-stable-index-ii/3904-smallest-stable-index-ii.java
+++ 3904-smallest-stable-index-ii/3904-smallest-stable-index-ii.java
@@ -1,11 +1,11 @@
 class Solution {
     public int firstStableIndex(int[] nums, int k) {
         int n = nums.length;
+        
         int[] minFromIndex = new int[n];
         minFromIndex[n-1] = nums[n-1];
-
         for(int i = n-2; i >= 0; i--){
-            minFromIndex[i] = Math.min(nums[i], minFromIndex[i+1]);
+            minFromIndex[i] = Math.min(minFromIndex[i+1], nums[i]);
         }
 
         int max = Integer.MIN_VALUE;
