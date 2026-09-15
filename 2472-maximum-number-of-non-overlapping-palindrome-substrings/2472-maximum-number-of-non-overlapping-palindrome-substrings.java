16123e716ac6643e8063d5a305e0c388fedebd99
Shameer Pathakota <shameerpathakota@gmail.com>
2026-09-15 19:39:23 +0530

    Time: 383 ms (5.03%), Space: 208.7 MB (9.05%) - LeetHub

--- 2472-maximum-number-of-non-overlapping-palindrome-substrings/2472-maximum-number-of-non-overlapping-palindrome-substrings.java
+++ 2472-maximum-number-of-non-overlapping-palindrome-substrings/2472-maximum-number-of-non-overlapping-palindrome-substrings.java
@@ -1,20 +1,30 @@
 class Solution {
     int n;
+    boolean[][] isPalindrome;
 
-    boolean isPalindrome(String s, int i, int j){
-        while(i < j){
-            if(s.charAt(i) != s.charAt(j)){
-                return false;
-            }
-            i++;
-            j--;
-        }
+    // boolean isPalindrome(String s, int i, int j){
+    //     while(i < j){
+    //         if(s.charAt(i) != s.charAt(j)){
+    //             return false;
+    //         }
+    //         i++;
+    //         j--;
+    //     }
 
-        return true;
-    }
+    //     return true;
+    // }
     public int maxPalindromes(String s, int k) {
         if(k == 1) return s.length();
         n = s.length();
+        isPalindrome = new boolean[n+1][n+1];
+        for(int L = 1; L <= n; L++){
+            for(int i = 0; i + L <= n; i++){
+                int j = L + i - 1;
+                if(i == j) isPalindrome[i][j] = true;//means substring of length 1
+                else if(i+1 == j) isPalindrome[i][j] = (s.charAt(i) == s.charAt(j));
+                else isPalindrome[i][j] = (s.charAt(i) == s.charAt(j) && isPalindrome[i+1][j-1]);
+            }
+        }
         int[][] dp = new int[n+1][n+1];
         for(int[] arr : dp){
             Arrays.fill(arr, -1);
@@ -30,7 +40,7 @@ class Solution {
         if(i >= n || j >= n) return 0;
         if(dp[i][j] != -1) return dp[i][j];
 
-        if(isPalindrome(s, i, j)){
+        if(isPalindrome[i][j]){
             int take_it = 1 + solve(s, k, j+1, j+k, dp);
             int grow_it = solve(s, k, i, j+1, dp);
             int slide_it = solve(s, k, i+1, j+1, dp);
@@ -44,4 +54,5 @@ class Solution {
         return dp[i][j] = Math.max(grow_it, slide_it);
 
     }
+    /*we can optimize the time complexity of O(n) for checking each time whether a substring is palindrome or not*/
 }
