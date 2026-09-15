00573662948100a449ad5cd93d2deb741fc08a8a
Shameer Pathakota <shameerpathakota@gmail.com>
2026-09-15 18:45:43 +0530

    Time: 2230 ms (5.03%), Space: 182.2 MB (9.55%) - LeetHub

---
+++ 2472-maximum-number-of-non-overlapping-palindrome-substrings/2472-maximum-number-of-non-overlapping-palindrome-substrings.java
@@ -0,0 +1,45 @@
+class Solution {
+    int n;
+
+    boolean isPalindrome(String s, int i, int j){
+        while(i < j){
+            if(s.charAt(i) != s.charAt(j)){
+                return false;
+            }
+            i++;
+            j--;
+        }
+
+        return true;
+    }
+    public int maxPalindromes(String s, int k) {
+        if(k == 1) return s.length();
+        n = s.length();
+        int[][] dp = new int[n+1][n+1];
+        for(int[] arr : dp){
+            Arrays.fill(arr, -1);
+        }
+
+        return solve(s, k, 0, k-1, dp);
+        
+    }
+
+    int solve(String s, int k, int i, int j, int[][] dp){
+        if(i >= n || j >= n) return 0;
+        if(dp[i][j] != -1) return dp[i][j];
+
+        if(isPalindrome(s, i, j)){
+            int take_it = 1 + solve(s, k, j+1, j+k, dp);
+            int grow_it = solve(s, k, i, j+1, dp);
+            int slide_it = solve(s, k, i+1, j+1, dp);
+
+            return dp[i][j] = Math.max(take_it, Math.max(grow_it, slide_it));
+        }
+
+        int grow_it = solve(s, k, i, j+1, dp);
+        int slide_it = solve(s, k, i+1, j+1, dp);
+
+        return dp[i][j] = Math.max(grow_it, slide_it);
+
+    }
+}
