a81d4be974a5747979fea586a4e30a23a60b0ba6
Shameer Pathakota <shameerpathakota@gmail.com>
2026-09-16 14:35:36 +0530

    Time: 20 ms (29.85%), Space: 46.7 MB (21.06%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.java
+++ 0647-palindromic-substrings/0647-palindromic-substrings.java
@@ -1,43 +1,23 @@
 class Solution {
-    int[][] dp;
-    //0 means false. 1 means true, -1 means not compute yet
     public int countSubstrings(String s) {
         int n = s.length();
-        dp = new int[n+1][n+1];
-        for(int[] arr : dp){
-            Arrays.fill(arr, -1);
-        }
         int count = 0;
-        for(int i = 0; i < n; i++){
-            for(int j = i; j < n; j++){
-                if(check(i, j, s)) count++;
+        boolean[][] dp = new boolean[n+1][n+1];
+
+        for(int L = 1; L <= n; L++){
+            for(int i = 0; i + L <= n; i++){
+                int j = L + i - 1;
+
+                if(i == j) dp[i][j] = true;
+                else if(i+1 == j) dp[i][j] = s.charAt(i) == s.charAt(j);
+                else dp[i][j] = (s.charAt(i) == s.charAt(j)) && dp[i+1][j-1];
+
+                if(dp[i][j]) count++;
+
             }
         }
 
         return count;
     }
-
-    boolean check(int i, int j, String s){
-        if(i > j){
-            return true;
-        } 
-        
-        if(dp[i][j] != -1){
-            return dp[i][j] == 1;
-        }
-
-        if(s.charAt(i) == s.charAt(j)){
-            boolean value = check(i+1, j-1, s);
-            if(value){
-                dp[i][j] = 1;
-            }
-            else{
-                dp[i][j] = 0;
-            }
-            return value;
-        } 
-
-        dp[i][j] = 0;
-        return false;
-    }
+    
 }
